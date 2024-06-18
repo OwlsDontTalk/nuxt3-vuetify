0e7ead92842097a394586c03fd3ea94ef2c03377
Ivan Miropolskii <miropolskiy.ivan@gmail.com>
2024-06-18 01:24:15 -0500

    initial commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="d-flex align-center flex-column pt-10">
+    <v-card class="intro-card">
+      <v-expansion-panels>
+        <v-expansion-panel
+            title="Vue3 & Vuetify Boilerplate"
+            text="Ready to go">
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-card>
+  </div>
+</template>
+<script lang="ts">
+</script>
+<style lang="scss">
+.intro-card {
+  width: 400px;
+}
+</style>
